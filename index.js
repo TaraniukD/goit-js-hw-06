40afc8bd8ae506643b74c5ce7fabcf0c38d61cff
Dmytro <109168390+TaraniukD@users.noreply.github.com>
2022-11-01 19:44:23 +0200

    test

---
+++ index.js
@@ -0,0 +1,15 @@
+const totalEl = document.querySelectorAll('.item')
+console.log('Number of categories:', totalEl.length);
+
+const titleName = document.querySelectorAll('h2')
+console.log(titleName);
+console.log(titleName.textContent);
+
+
+
+const list = titleName.firstElementChild;
+console.log(list);
+
+
+
+
